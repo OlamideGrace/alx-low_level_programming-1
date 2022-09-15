621036102f1a9da855858c6a63670a5bc3536989
Ayomide <oyetimehin31@gmail.com>
2022-09-15 21:32:51 +0100

    C_More_functions_more_nested_loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * mul - return multiplication of a * b
- * @a: first number to multiply
- * @b: secondary number to multiply
- * Return: multiplication of a * b
+ * mul - returns the product of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
  */
 
 int mul(int a, int b)
